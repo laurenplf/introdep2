1a1701061d06154e8fc4d69b45d0ff9c7a9bdd8a
Pierre-Louis Laurent <lauren272u@a-mn-p322-pc09.artem.site.univ-lorraine.fr>
2018-06-20 17:42:38 +0200

    commit h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int mylen(char s[]);
+void create_str(char s[], int num);
+int compare(char s1[], char s2[]);
+int is_pal(char s[]);
+
+#endif // MAIN_H_INCLUDED
